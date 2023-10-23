807be84948621f25602a4b80e575bca952c4783d
BashMagno <alejandrohigueramoreno@gmail.com>
2023-10-23 02:38:50 +0200

    Cambios en el codigo

--- ipInfobyMango.py
+++ ipInfobyMango.py
@@ -1,13 +1,7 @@
 import os
 import ipinfo
 import pprint
-import sys
 from colorama import Fore, Style, init
-
-####################
-# CREATED BY MANGO #
-####################
-
 
 # Inicializar colorama
 init()
@@ -17,16 +11,25 @@ def get_ip_details(api_token, ip_address):
     details = handler.getDetails(ip_address)
     return details.all
 
-if __name__ == '__main__':
+def scan_ip():
     api_token = os.environ.get('IPINFO_API_TOKEN')
     
     if not api_token:
         print(f"{Fore.RED}La variable de entorno IPINFO_API_TOKEN no está configurada.{Style.RESET_ALL}")
-        sys.exit(1)
-
+        return
+    
     ip_address = input(f"{Fore.MAGENTA}Por favor, ingresa la dirección IP: {Fore.RESET}")
 
     details_all = get_ip_details(api_token, ip_address)
     
     # Imprimir los detalles en morado
-    pprint.pprint(details_all, indent=4, width=80, color=Fore.MAGENTA)
+    print(f"{Fore.MAGENTA}")
+    pprint.pprint(details_all, indent=4, width=80)
+    print(f"{Style.RESET_ALL}")
+
+if __name__ == '__main__':
+    while True:
+        scan_ip()
+        choice = input(f"{Fore.YELLOW}¿Deseas escanear otra IP? (s/n): {Fore.RESET}")
+        if choice.lower() != 's':
+            break
